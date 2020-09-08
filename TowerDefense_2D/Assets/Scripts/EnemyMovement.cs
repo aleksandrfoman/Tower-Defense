42394bb9f4a801ad699f940e31bb454ba35fa559
aleksandrfoman <aleksandrfoman@gmail.com>
2020-09-08 09:35:59 +0300

    .адд

---
+++ TowerDefense_2D/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Transform target;
+    private int wavepointIndex = 0;
+    private EnemyScr enemyScr;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+        enemyScr = GetComponent<EnemyScr>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            enemyScr.EndPath();
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
